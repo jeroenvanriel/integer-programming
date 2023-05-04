1d567f48c941c1e0bf5a087a53899538f0125f8c
Jeroen van Riel <jeroenvanriel@outlook.com>
2023-05-04 16:02:35 +0200

    add all assignment 1 files

---
+++ assignment-1/template_column_generation.py
@@ -0,0 +1,174 @@
+import sys
+import gurobipy as gp
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+###############################################################################################
+##### HEADER ####
+###############################################################################################
+
+# Group Members:
+#
+# Student 1:
+# Student 2:
+# ...
+
+######################################
+# AUXILIARY FUNCTIONS
+######################################
+
+def read_instance(filename):
+    '''
+    reads an instance of the polyhedral classifier problem
+
+    filename - path to file containing instance
+    '''
+
+    f = open(filename, 'r')
+
+    K = -1
+    X = []
+    Y = []
+
+    for line in f:
+        if line.startswith('k'):
+            K = int(line.split()[1])
+        elif line.startswith('x'):
+            point = [int(i) for i in line.split()[1:]]
+            X.append(point)
+        elif line.startswith('y'):
+            point = [int(i) for i in line.split()[1:]]
+            Y.append(point)
+
+    f.close()
+
+    return K, X, Y
+
+######################################
+# EXERCISE 1
+######################################
+
+def exercise1(
+        K, X, Y, eps
+        ):
+
+    M = 10
+    n = len(X[0])
+    RX = len(X)
+    RY = len(Y)
+    m = gp.Model()
+
+    plot_data(X, Y)
+    
+    u = [m.addVar(obj=1, vtype=gp.GRB.BINARY, name=f"u_{i}") for i in range(K)]
+    a = { (i, k): m.addVar(-float('inf'), float('inf'), obj=0, vtype=gp.GRB.CONTINUOUS, name="a_{}_{}".format(i,k))
+            for k in range(n)
+            for i in range(K) }
+    b = [m.addVar(obj=0, vtype=gp.GRB.CONTINUOUS, name=f"b_{i}") for i in range(K)]
+    s = { (i, j): m.addVar(obj=0, vtype=gp.GRB.BINARY, name="s_{}_{}".format(i,j)) for i in range(K) for j in range(RY) }
+
+    # sign variables for a_i
+    p = { (i, k): m.addVar(obj=0, vtype=gp.GRB.BINARY, name=f"p_{i}_{k}") for i in range(K) for k in range(n) }
+    # sign constraints for a_i
+    for i in range(K):
+        for k in range(n):
+            m.addConstr(a[i,k] + p[i,k] * M >= 1)
+            m.addConstr(a[i,k] - (1 - p[i,k]) * M <= -1)
+
+    cx = [m.addConstr(gp.quicksum(a[i,k] * X[j][k] for k in range(n)) <= b[i]) for j in range(RX) for i in range(K)]
+    cy = [m.addConstr(M * (s[i,j] + 1 - u[i]) + gp.quicksum(a[i,k] * Y[j][k] for k in range(n)) >= b[i] + eps)
+                            for j in range(RY)
+                            for i in range(K)
+                            ]
+    cu = [m.addConstr(gp.quicksum(s[i,j] for i in range(K)) <= gp.quicksum(u[i] for i in range(K)) - 1) for j in range(RY)]
+
+    m.ModelSense = gp.GRB.MINIMIZE
+    m.update()
+    m.optimize()
+
+    # print(u)
+    # print(s)
+    # print(a)
+    # print(b)
+
+    # sample points from lines
+    step = 0.5
+    xs = np.arange(-1, 2, step)
+    for i in range(K):
+        if i == 6 or u[i].X < 0.5:
+            continue
+        if abs(a[i, 1].X) < eps / 2:
+            # horizontal line
+            ys = [0 for x in xs]
+        else:
+            ys = [(b[i].X - a[i,0].X * x) / a[i,1].X for x in xs]
+        plt.plot(xs, ys, linestyle=('dotted' if u[i].X < 0.5 else 'solid'))
+    plt.show()
+
+
+def plot_data(X, Y):
+    xs = np.array(X)
+    ys = np.array(Y)
+    plt.scatter(xs[:,0], xs[:,1], marker='x')
+    plt.scatter(ys[:,0], ys[:,1], marker='o')
+
+######################################
+# EXERCISE 3
+######################################
+
+def exercise3(
+        # provide input here
+        ):
+    pass
+
+
+##############################
+# EXERCISE 4
+##############################
+
+def exercise4(
+        # provide input here
+        ):
+    pass
+
+
+##############################
+# MAIN FUNCTION
+##############################
+
+def main():
+
+    path = None
+    if len(sys.argv) > 1:
+        path = pathlib.Path(sys.argv[1])
+    else:
+        path = pathlib.Path("data_cg_cube.cg")
+        print(f"WARNING: Instance not specified. Defaulting to {path}.")
+    path = pathlib.Path(r"./data_cg_cross4.cg")
+    assert path.exists(), f"Path {path} does not exist. ({path.resolve()})"
+    
+    print(f"Reading instance {path}")
+
+    K, X, Y = read_instance(path)
+    eps = 0.001
+
+    ###############################################
+    # only modify this function within this block
+    ###############################################
+
+    # add your input to the following functions
+
+    exercise1(K, X, Y, eps)
+
+    exercise3()
+
+    exercise4()
+
+    ###############################################
+    # end of modifiable block
+    ###############################################
+
+if __name__ == "__main__":
+    main()
