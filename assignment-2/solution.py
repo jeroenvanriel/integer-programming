f604591bc916b13bb8cb6a91b2f0fee231250187
K. Moors <k.moors@student.tue.nl>
2023-06-02 12:50:01 +0200

    Implemented dijkstra using networks

--- assignment-2/solution.py
+++ assignment-2/solution.py
@@ -2,6 +2,7 @@ import sys
 import gurobipy as gp
 import pathlib
 import numpy as np
+import networkx as nx
 
 ###############################################################################################
 ##### HEADER ####
@@ -35,16 +36,37 @@ def read_instance(filename):
 
     return edges
 
-class Graph:
+def edit_path(path, n):
+    """Given a path, it finds the shortest cycle in it. Moreover, reduces all elements modulo n.
 
-    def __init__(self, edges, x):
-        self.n = max(max(e) for e in edges)
+    Examples:
+    edit_path([0,1,2,3,0], 10)
+    >>> [0,1,2,3]
+    edit_path([0,1,2,3,1,0], 10)
+    >>> [1,2,3]
+    edit_path([0,11,2,13,1,10], 10)
+    >>> [1,2,3]
+    """
+    path = [x % n for x in path]
+    while path[0] == path[-1]:
+        x = path[0]
+        path = path[1:-1]
+    return [x] + path
+
+
+class MyGraph:
+
+    def __init__(self, edges):
+        self.n = max(max(e) for e in edges) + 1
         self.m = len(edges)
         self.edges = edges
         self.V = np.arange(2*self.n)
         self.E = np.zeros(shape=(2*self.n, 2*self.n))
 
     def update_weights(self, x):
+        for i in range(2*self.n):
+            for j in range(2*self.n):
+                self.E[i,j] = np.infty
         for (e0, e1) in self.edges:
             w = 1 - x[e0] - x[e1]
             self.E[e0,e1+self.n] = w
@@ -67,7 +89,7 @@ class Graph:
  
         # Search not nearest vertex not in the
         # shortest path tree
-        for v in self.V:
+        for v in range(self.n*2):
             if dist[v] < min and sptSet[v] == False:
                 min = dist[v]
                 min_index = v
@@ -78,18 +100,20 @@ class Graph:
     # shortest path algorithm for a graph represented
     # using adjacency matrix representation
     def dijkstra(self, src):
- 
+        
         dist = [1e7] * self.n * 2
         dist[src] = 0
         sptSet = [False] * self.n * 2
  
-        for cout in self.V:
+        for cout in range(self.n*2):
  
             # Pick the minimum distance vertex from
             # the set of vertices not yet processed.
             # u is always equal to src in first iteration
-            u = self.minDistance(dist, sptSet)
- 
+            try:
+                u = self.minDistance(dist, sptSet)
+            except UnboundLocalError:
+                continue
             # Put the minimum distance vertex in the
             # shortest path tree
             sptSet[u] = True
@@ -98,18 +122,18 @@ class Graph:
             # of the picked vertex only if the current
             # distance is greater than new distance and
             # the vertex in not in the shortest path tree
-            for v in self.V:
-                if (self.graph[u][v] > 0 and
+            for v in range(self.n*2):
+                if (self.E[u][v] > 0 and
                    sptSet[v] == False and
-                   dist[v] > dist[u] + self.graph[u][v]):
-                    dist[v] = dist[u] + self.graph[u][v]
+                   dist[v] > dist[u] + self.E[u][v]):
+                    dist[v] = dist[u] + self.E[u][v]
 
-        self.printSolution(dist)
+        # self.printSolution(dist)
         return dist
 
     def parallel_distances(self):
         dist = np.zeros(self.n)
-        for v in self.V:
+        for v in range(self.n):
             dist[v] = self.dijkstra(v)[v+self.n]
         return dist
     
@@ -134,17 +158,18 @@ def exercise5(edges):
     m.Params.PreCrush = 1
 
     ###########################
-    G = Graph(edges, x=1)
-    x = m.addVars(range(G.n), vtype=gp.GRB.BINARY, name="x")
-    m.addConstrs(
-        x[i] + x[j] <= 1 for (i, j) in G.edges
-        ) 
+    n = max(max(e) for e in edges) + 1
+
+    x = m.addVars(range(n), vtype=gp.GRB.BINARY, name="x")
+    for (i,j) in edges:
+        m.addConstr(x[i] + x[j] <= 1)
 
     m.setObjective(
-        gp.quicksum(1 * x[i] for i in range(G.n)),
+        gp.quicksum(1 * x[i] for i in range(n)),
         sense=gp.GRB.MAXIMIZE
     )
-    
+
+    G = MyGraph(edges)
     
     def callback(model, where):
         if where == gp.GRB.Callback.POLLING:
@@ -165,9 +190,25 @@ def exercise5(edges):
             print(f"# Solved branch-and-bound tree node to optimality. Search violated cover inequality:")
 
             # Find a violated cover inequality
-            G.update_weights(x_frac)
-            ind = G.find_cycle()
-            cut = gp.quicksum(x[i] for i in ind) <= (len(ind) - 1) / 2
+            G: nx.Graph = nx.Graph()
+            for (i,j) in edges:
+                w = 1 - x_frac[i] - x_frac[j]
+                w = min(w , 1)
+                w = max(w, 0)
+                G.add_edge(u_of_edge=i, v_of_edge=j+n, weight=w)
+                G.add_edge(u_of_edge=i+n, v_of_edge=j, weight=w)
+
+            min_l = np.infty
+            for i in range(n):
+                l = nx.dijkstra_path_length(G, source=i, target=i+n, weight="weight")
+                if l < min_l:
+                    min_l = l
+                    min_i = i
+
+            path = nx.dijkstra_path(G, source=min_i, target=min_i+n, weight="weight")
+            path = edit_path(path, n)   # first and last element are the same
+
+            cut = gp.quicksum(x[i] for i in path) <= (len(path) - 1) / 2
             if cut is None:
                 print("Search is unsuccessful")
                 return
@@ -181,14 +222,6 @@ def exercise5(edges):
             # get the solution value; see https://www.gurobi.com/documentation/9.5/refman/cb_codes.html#sec:CallbackCodes
             x_sol_val = model.cbGet(gp.GRB.Callback.MIPSOL_OBJ)
 
-            print()
-            print(f"# Found new solution with value {x_sol_val:.2f}")
-            items = [i for i in range(G.n) if x_sol[i] > .9]
-            for i in range(G.n):
-                if x_sol[i] < .5:
-                    continue
-                print(f"{i} (value {x_sol[i]:.1f})")
-            return
 
         pass
 
